7000e6f54114522662250d9b37024b1edc42f5c4
chocopie116@gmail.com <t.kajinuma@gmail.com>
2014-02-27 02:50:13 +0900

    room

--- app.js
+++ app.js
@@ -27,29 +27,25 @@ var chatHistory = [];
 //チャット
 var chat = io.of('/chat').on('connection', function(client) {
     client.on('from_client', function(data) {
+
         var message  =  getLineMessage(data.name +  'さん : ' + data.message);
         chatHistory.push(message);
         //接続ソケット全て(=自分含む全員)
-        io.of('/chat').emit('from_server', message);
+        chat.emit('from_server', {message: message});
     });
 });
 
-//入退室
+var attendance = 0;
 var room = io.of('/room').on('connection', function(client) {
     client.on('entered', function() {
-        //接続ソケットのみ(=自分のみ)
-        var messages = chatHistory;
-        messages.push(getLineMessage("挨拶をしてみましょう"));
-        client.emit('someone_entered', messages);
-        //接続ソケット以外(=自分以外)
-        var array = [];
-        array.push(getLineMessage('誰かが入室しました'));
-        client.broadcast.emit('someone_entered', array);
+        attendance++;
+        room.emit('from_server', attendance);
+        io.of('/chat').emit('from_server', chatHistory);
     });
 
     client.on('disconnect', function() {
-        //接続ソケット以外(=自分以外)
-        client.broadcast.emit('someone_left', getLineMessage('誰かが退室しました。'));
+        attendance--;
+        room.emit('from_server', attendance);
     });
 });
 
